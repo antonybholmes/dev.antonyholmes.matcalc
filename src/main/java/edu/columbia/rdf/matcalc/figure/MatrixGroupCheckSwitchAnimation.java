74fd230c56d81f087a3feee0830f29de06fbc9b2
Antony Holmes <antony@antonyholmes.dev>
2024-02-26 17:26:21 -0500

    fix: Initial

---
+++ src/main/java/edu/columbia/rdf/matcalc/figure/MatrixGroupCheckSwitchAnimation.java
@@ -0,0 +1,13 @@
+package edu.columbia.rdf.matcalc.figure;
+
+import java.awt.Color;
+
+import org.jebtk.modern.ModernWidget;
+import org.jebtk.modern.button.CheckSwitchAnimation;
+
+public class MatrixGroupCheckSwitchAnimation extends CheckSwitchAnimation {
+
+  public MatrixGroupCheckSwitchAnimation(ModernWidget widget, Color color) {
+    super(widget, color);
+  }
+}
